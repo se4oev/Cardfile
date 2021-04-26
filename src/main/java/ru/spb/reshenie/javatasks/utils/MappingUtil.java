6668489e66ed536a9e0c430a56284f811bf62190
se4oev <p.karpenko26@gmail.com>
2021-04-26 11:35:53 +0300

    Add icons.

--- src/main/java/ru/spb/reshenie/javatasks/utils/MappingUtil.java
+++ src/main/java/ru/spb/reshenie/javatasks/utils/MappingUtil.java
@@ -5,6 +5,7 @@ import javafx.scene.image.ImageView;
 import ru.spb.reshenie.javatasks.entity.Patient;
 import ru.spb.reshenie.javatasks.entity.PatientDTO;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
